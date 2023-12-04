55b92933b6e9ad00a034c9ab734c3133dc74ebae
Anuj Kumar Loomba <anujloomba@gmail.com>
2023-12-04 23:39:20 +1100

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,6 @@ from langchain.text_splitter import CharacterTextSplitter
 import os
 import getpass
 
-os.environ['OPENAI_API_KEY'] = getpass.getpass('OpenAI API Key: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')
-
 loader = DirectoryLoader(".\Kanoon", glob="**/*.pdf", show_progress=True, use_multithreading=True, silent_errors=True)
 docs = loader.load()
 text_splitter = CharacterTextSplitter(
